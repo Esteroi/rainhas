59b021042d8072e3c4dff8e1251595f7de827ac8
Esteroi <oioli.ester@gmail.com>
2025-10-09 13:54:32 -0300

    promocoes.js

--- util/promocoes.js
+++ util/promocoes.js
@@ -1,35 +1,6 @@
 function mensagemPromocoes() {
   return `
 
-*👑 RAINHAS DA SORTE 👑*
-<<<<<<< HEAD
-🔔 * PROMOÇÕES SURPRESA* 🔔
-
-*🚨GANHA NO PIX🚨*
-
-*_Coloca 15 OU MAIS de recarga,_* 
-
-*_RECEBE 5 DE VOLTA NO PIX_*
-
-
-🔹CLUBE 777: https://777asian.cc/?id=181355692
-OUUU
-🔹COROA: https://www.jjzzjogo.com/?id=741444131
-OUUUUU
-🔹MANGA: https://manga-cupcake-pg/?id=672769741
-
-
-*CHAME ADM ESTER OU ADM KELY para aproveitar e faturar*
-
-⏰ Válido só AGORA! 
-Corre antes que acabe! 🏃‍♂️💨
-POUCAS VAGAS
-
-É isso mesmo!! 💸🔥
-*vagas LIMITADAS
-🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦
-*_As promoções são um complemento para aumentar as chances e aproveitar melhor a experiência._ *     
-=======
 
 🔔 * PROMOÇÕES SURPRESA!* 🔔
 
@@ -60,7 +31,6 @@ POUCAS VAGAS
 *vagas LIMITADAS
 🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦
 *_As promoções são um complemento para aumentar as chances e aproveitar melhor a experiência._*     
->>>>>>> 3312338f92c6eeaadad4b9e9a97a95dd8678e452
 🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦🧦
 `;
 }
