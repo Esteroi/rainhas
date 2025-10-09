1a83567d1ed6e03b640238159f0004ba67cdc2d6
Esteroi <oioli.ester@gmail.com>
2025-10-09 14:33:11 -0300

    plataformas.js

--- util/plataformas.js
+++ util/plataformas.js
@@ -4,40 +4,43 @@ function mensagemPlataformas() {
 
 🎯 *PLATAFORMAS INDICADAS* 🎯
 
+✅ **MANGA CUPCAKE**
+🔗 https://manga-Cupcake-pg.com/?id=672769741
 
-✅**MANGA CUPCAKE*
-🔗https://manga-Cupcake-pg.com/?id=672769741
+✅ **CLUBE 777 ASIA**
+🔗 https://777asian.cc/?id=181355692
 
-✅**CLUBE 777 ASIA*
-🔗https://777asian.cc/?id=181355692
+✅ **COROA JJZZ**
+🔗 https://www.jjzzjogo.com/?id=741444131
 
-✅**COROA JJZZ*
-🔗https://www.jjzzjogo.com/?id=741444131
+✅ **MK SLOTS**
+🔗 https://wwww.slotsmk.bet/?id=567149395
 
-✅**MK SLOTS*
-🔗https://wwww.slotsmk.bet/?id=567149395
+✅ **OKOK COCO**
+🔗 https://wwww.okokcoco.win/?id=366139391
 
-✅**OKOK COCO*
-🔗https://wwww.okokcoco.win/?id=366139391
-
-⚠️ *Atenção:*⚠️
-Sempre invista com cautela e responsabilidade.  
-🍀 Boa sorte!`;
-}
-function mensagemPlataformasParaDicas() {
-  return `\n\n💡 *Plataformas indicadas para aplicar esta dica:*  
-🔹CLUBE 777: https://777asian.cc/?id=181355692
-🔹COROA: https://www.jjzzjogo.com/?id=741444131
-🔹MANGA: https://manga-cupcake-pg/?id=672769741
-🔹MK: https://wwww.slotsmk.bet/?id=567149395
-🔹OKOK: https://wwww.okokcoco.win/?id=366139391`;
-⚠️ *Atenção:*⚠️
+⚠️ *Atenção:* ⚠️
 Sempre invista com cautela e responsabilidade.  
 🍀 Boa sorte!
 `;
 }
+
+function mensagemPlataformasParaDicas() {
+  return `
+💡 *Plataformas indicadas para aplicar esta dica:*  
+🔹 CLUBE 777: https://777asian.cc/?id=181355692
+🔹 COROA: https://www.jjzzjogo.com/?id=741444131
+🔹 MANGA: https://manga-cupcake-pg.com/?id=672769741
+🔹 MK: https://wwww.slotsmk.bet/?id=567149395
+🔹 OKOK: https://wwww.okokcoco.win/?id=366139391
+
+⚠️ *Atenção:* ⚠️
+Sempre invista com cautela e responsabilidade.  
+🍀 Boa sorte!
+`;
+}
+
 module.exports = {
   mensagemPlataformas,
   mensagemPlataformasParaDicas,
-
 };
