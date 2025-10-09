f9d84a0e93a65cc0db5407613131fa2d9488eb8a
Esteroi <oioli.ester@gmail.com>
2025-10-09 13:53:04 -0300

    plataformas.js

--- util/plataformas.js
+++ util/plataformas.js
@@ -5,21 +5,14 @@ function mensagemPlataformas() {
 🎯 *PLATAFORMAS INDICADAS* 🎯
 
 
-<<<<<<< HEAD
-=======
 ✅**MANGA CUPCAKE*
 🔗https://manga-Cupcake-pg.com/?id=672769741
->>>>>>> 3312338f92c6eeaadad4b9e9a97a95dd8678e452
 
 ✅**CLUBE 777 ASIA*
 🔗https://777asian.cc/?id=181355692
 
 ✅**COROA JJZZ*
 🔗https://www.jjzzjogo.com/?id=741444131
-
-<<<<<<< HEAD
-✅**MANGA CUPCAKE*
-🔗https://manga-cupcake-pg/?id=672769741
 
 
 ⚠️ *Atenção:*⚠️
@@ -31,25 +24,13 @@ function mensagemPlataformasParaDicas() {
 🔹CLUBE 777: https://777asian.cc/?id=181355692
 🔹COROA: https://www.jjzzjogo.com/?id=741444131
 🔹MANGA: https://manga-cupcake-pg/?id=672769741`;
-=======
 ⚠️ *Atenção:*⚠️
 Sempre invista com cautela e responsabilidade.  
 🍀 Boa sorte!
 `;
 }
-function mensagemPlataformasParaDicas() {
-  return `\n\n💡 *Plataformas indicadas para aplicar esta dica:*  
-🔹MANGA: https://manga-Cupcake-pg.com/?id=672769741
-🔹CLUBE 777: https://777asian.cc/?id=181355692
-🔹COROA: https://www.jjzzjogo.com/?id=741444131`;
->>>>>>> 3312338f92c6eeaadad4b9e9a97a95dd8678e452
-}
-
 module.exports = {
   mensagemPlataformas,
   mensagemPlataformasParaDicas,
-<<<<<<< HEAD
+
 };
-=======
-};
->>>>>>> 3312338f92c6eeaadad4b9e9a97a95dd8678e452
