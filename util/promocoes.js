7a789bb8965f29bed0610b6fc179bd707cd2b296
Esteroi <oioli.ester@gmail.com>
2025-10-22 19:11:16 -0300

    promocoes.js

--- util/promocoes.js
+++ util/promocoes.js
@@ -18,6 +18,16 @@ https://999kitty.bet/?id=347692110
 
 https://tyocelot.com?a=779626341
 
+https://www.2opg.net/?id=889918974
+
+https://777ski.com/?id=811690644
+
+https://m1-CCC.bet/?id=336507100
+
+https://kfs5555.com?a=448462147
+
+https://manga-magico-pg.com/?id=642054731
+
 
 *POUCAS VAGAS*
 
